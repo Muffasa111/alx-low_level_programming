03b8f21924cb874a5e3f7b12d688fdeb2d14b21a
Muffasa111 <njorogedwin03@gmail.com>
2023-08-14 15:19:59 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#!/bin/bash
-#inlude<stdio.h>
+#include<stdio.h>
 /**
- * main - Entry point
+ *  main - Entry point 
  *
- * Return Always (0) (Success)
-int main (void) 
+ *  return Always 0 (Success)
+ */
+int main (void)
 {
-	puts("/"Programming is like building a multilingual puzzle")
-	return (0)
+	puts("/"Programming is like building a multilingual puzzle");
+	return (0);
 }
